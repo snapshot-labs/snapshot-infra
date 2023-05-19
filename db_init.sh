386b6df48af03cb7f863be64414a162cd202f81a
Dmytro Tolok <tolok.d@gmail.com>
2023-05-19 20:00:02 +0200

    feat(db): add simple migration mechanism

---
+++ db_init.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# read and set environment variables
+set -a
+source .env
+set +a
+
+database_relayer="snapshot-relayer"
+database_sequencer="snapshot-sequencer"
+database_keycard="keycard"
+
+database_relayer_schema="$SNAPSHOT_RELAYER_REPO_PATH/src/schema.sql"
+database_sequencer_schema="$SNAPSHOT_SEQUENCER_REPO_PATH/src/helpers/schema.sql"
+database_keycard_schema="$KEYCARD_REPO_PATH/src/helpers/schema.sql"
+
+dist_path="./schemas"
+
+transform_sql_file() {
+  local database_relayer="$1"
+  local database_relayer_schema="$2"
+
+  local temp_file="$(mktemp)"
+
+  printf "CREATE DATABASE IF NOT EXISTS \`$database_relayer\`;\n" | cat - "$database_relayer_schema" > "$temp_file"
+
+  sed "s/CREATE TABLE /CREATE TABLE \`$database_relayer\`./g" "$temp_file" > "$dist_path/$database_relayer.sql"
+}
+
+transform_sql_file $database_relayer $database_relayer_schema
+transform_sql_file $database_sequencer $database_sequencer_schema
+transform_sql_file $database_keycard $database_keycard_schema
+
---
+++ schemas/keycard.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE IF NOT EXISTS `keycard`;
+CREATE TABLE `keycard`.`keys` (
+  `key` VARCHAR(256) DEFAULT NULL,
+  owner VARCHAR(64) NOT NULL,
+  name VARCHAR(32) NOT NULL,
+  created INT(11) DEFAULT (UNIX_TIMESTAMP()),
+  updated INT(11) DEFAULT (UNIX_TIMESTAMP()),
+  active BOOLEAN NOT NULL DEFAULT TRUE,
+  PRIMARY KEY (`owner`),
+  INDEX `key` (`key`),
+  INDEX owner (owner),
+  INDEX created (created)
+);
+
+CREATE TABLE `keycard`.reqs (
+  `key` VARCHAR(256) NOT NULL,
+  app VARCHAR(32) NOT NULL,
+  total INT(12) NOT NULL DEFAULT 0,
+  last_active INT(11) DEFAULT (UNIX_TIMESTAMP()),
+  PRIMARY KEY (`key`, app),
+  INDEX total (total)
+);
+
+CREATE TABLE `keycard`.reqs_daily (
+  `key` VARCHAR(256) NOT NULL,
+  app VARCHAR(32) NOT NULL,
+  day VARCHAR(32) NOT NULL,
+  total INT(12) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`key`, day, app),
+  INDEX total (total)
+);
+
+CREATE TABLE `keycard`.reqs_monthly (
+  `key` VARCHAR(256) NOT NULL,
+  app VARCHAR(32) NOT NULL,
+  month VARCHAR(32) NOT NULL,
+  total INT(12) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`key`, month, app),
+  INDEX total (total)
+);
