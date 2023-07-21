83f40a2f115a0536b3790e29a4074aa248c2886c
Dmytro Tolok <tolok.d@gmail.com>
2023-07-21 16:11:55 +0200

    fix(brovider): refresh mysql schema

--- schemas/brovider.sql
+++ schemas/brovider.sql
@@ -6,21 +6,12 @@ CREATE TABLE `brovider`.nodes (
   `requests` INT(24) NOT NULL DEFAULT 0,
   `errors` INT(24) NOT NULL DEFAULT 0,
   `duration` INT(24) NOT NULL DEFAULT 0,
-  `archive` TINYINT(1) NOT NULL DEFAULT 0,
+  `archive` TINYINT(1) NOT NULL DEFAULT -1,
+  `multicall` VARCHAR(256) NOT NULL,
   `created` INT(11) NOT NULL,
   PRIMARY KEY (`url`),
   INDEX `network` (`network`),
   INDEX `provider` (`provider`),
   INDEX `archive` (`archive`),
   INDEX `created` (`created`)
-);
-
-CREATE TABLE `brovider`.requests (
-  `network` VARCHAR(24) NOT NULL,
-  `method` VARCHAR(512) NOT NULL,
-  `archive` INT(1) NOT NULL,
-  `cache` INT(1) NOT NULL,
-  `count` INT(24) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`network`, `method`, `archive`, `cache`),
-  INDEX `count` (`count`)
 );
